0e411d638765ee154f4c437f416670666eb85157
h.kuwabara <h.kuwabara1103@gmail.com>
2023-01-14 23:50:25 +0900

    secretsファイルを作ってそこにAPIKeyを保存させる

--- lib/features/map/data/map_repository.dart
+++ lib/features/map/data/map_repository.dart
@@ -6,6 +6,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../configs/secrets.dart';
+
 final mapRepositoryProvider = Provider(
   (ref) => MapRepository(),
 );
@@ -15,8 +17,15 @@ class MapRepository {
 
   Future<void> searchFacilities(String word) async {
     // ToDo ちゃんとClient設計したい
-    final response = await http.get(Uri.https('maps.googleapis.com', '/maps/api/place/autocomplete/json',
-        {'input': word, 'types': 'lodging', 'key': 'ApiKey'}));
+    print(googleMapAPIKey);
+    print(word);
+    final response = await http.get(
+      Uri.https(
+        'maps.googleapis.com',
+        '/maps/api/place/autocomplete/json',
+        {'input': word, 'types': 'lodging', 'key': googleMapAPIKey},
+      ),
+    );
     final jsonResponse = jsonDecode(response.body);
     print('json: ${jsonResponse}');
   }
