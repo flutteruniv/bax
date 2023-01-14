78fba9b1e87f29de72cd248473a8d32a57fad0b7
h.kuwabara <h.kuwabara1103@gmail.com>
2023-01-15 01:14:53 +0900

    Responseクラス作成

--- lib/features/map/data/map_repository.dart
+++ lib/features/map/data/map_repository.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:bax/configs/firebase.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../configs/secrets.dart';
+import '../domain/facility_prediction_results.dart';
 
 final mapRepositoryProvider = Provider(
   (ref) => MapRepository(),
@@ -17,16 +16,17 @@ class MapRepository {
 
   Future<void> searchFacilities(String word) async {
     // ToDo ちゃんとClient設計したい
-    print(googleMapAPIKey);
-    print(word);
     final response = await http.get(
       Uri.https(
         'maps.googleapis.com',
         '/maps/api/place/autocomplete/json',
-        {'input': word, 'types': 'lodging', 'key': googleMapAPIKey},
+        {'input': word, 'types': 'lodging', 'key': googleMapAPIKey, 'language': 'ja'},
+
+        /// Todo: languageにOSの言語を設定したい。jaを設定しないと住所だけ英語で返ってくる
       ),
     );
-    final jsonResponse = jsonDecode(response.body);
-    print('json: ${jsonResponse}');
+    final jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+    final results = FacilityPredictionResults.fromJson(jsonResponse);
+    print('results: ${results}');
   }
 }
