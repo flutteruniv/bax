1cff1be58761115015162fa4c8c7b84c28b623a7
h.kuwabara <h.kuwabara1103@gmail.com>
2023-01-19 09:16:22 +0900

    リネーム

--- lib/features/map/application/map_service.dart
+++ lib/features/map/application/map_service.dart
@@ -10,7 +10,7 @@ import '../domain/selected_location_info.dart';
 /// ユーザーが施設予測結果から選択したときにロケーション情報が返される[StreamProvider]
 final selectedLocationInfoStreamProvider = StreamProvider(
   (ref) {
-    return ref.watch(mapServiceProvider).locationInfoStream();
+    return ref.watch(mapServiceProvider).selectedLocationInfoStream();
   },
 );
 
@@ -26,10 +26,10 @@ class MapService {
   /// 保留中の検索文字列
   String? _holdQuery;
 
-  final _locationInfoController = StreamController<SelectedLocationInfo>();
+  final _selectedLocationInfoController = StreamController<SelectedLocationInfo>();
 
-  Stream<SelectedLocationInfo> locationInfoStream() {
-    return _locationInfoController.stream;
+  Stream<SelectedLocationInfo> selectedLocationInfoStream() {
+    return _selectedLocationInfoController.stream;
   }
 
   Future<void> searchFacilities(String query, String localeLanguage) async {
@@ -50,7 +50,7 @@ class MapService {
   Future<void> geocoding(String facilityId, String name) async {
     final facility = await ref.watch(facilityRepositoryProvider).fetchFacility(facilityId);
     if (facility != null) {
-      _locationInfoController.add(
+      _selectedLocationInfoController.add(
         SelectedLocationInfo(
           facilityId: facilityId,
           name: name,
@@ -63,7 +63,7 @@ class MapService {
 
     /// Firestoreに該当のデータがない場合はApiから取得する
     final location = await ref.watch(mapRepositoryProvider).geocoding(facilityId);
-    _locationInfoController.add(
+    _selectedLocationInfoController.add(
       SelectedLocationInfo(
         facilityId: facilityId,
         name: name,
