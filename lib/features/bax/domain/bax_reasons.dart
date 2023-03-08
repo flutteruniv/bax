e4c9108a764343bea27e200987d167cf876fedd4
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-03-09 08:22:15 +0900

    bax付与の理由サンプルを追加

---
+++ lib/features/bax/domain/bax_reasons.dart
@@ -0,0 +1,17 @@
+import 'bax_reason.dart';
+
+class BaxReasons {
+  BaxReasons._();
+
+  /// Wi-Fi測定時の基礎ポイント
+  static const measurementWifi = BaxReason(
+    text: 'Wi-Fi測定',
+    point: 10,
+  );
+
+  /// その施設に対して初の投稿の場合
+  static const findingNewWiFi = BaxReason(
+    text: '新Wi-Fiスポット発見',
+    point: 50,
+  );
+}
