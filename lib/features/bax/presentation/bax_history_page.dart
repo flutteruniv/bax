2d0b5596e9561cf3fada0cbffa473f9b2f4646db
h.kuwabara <h.kuwabara1103@gmail.com>
2023-03-18 22:20:58 +0900

    履歴画面のUIを仮実装

--- lib/features/bax/presentation/bax_history_page.dart
+++ lib/features/bax/presentation/bax_history_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../common_widgets/bax_indicator.dart';
+import '../application/bax_service.dart';
+
 class BaxHistoryPage extends ConsumerStatefulWidget {
   const BaxHistoryPage({super.key});
 
@@ -15,7 +18,36 @@ class _BaxHistoryPageState extends ConsumerState<BaxHistoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bax履歴')),
-      body: Text('履歴'),
+      body: ref.watch(baxHistoriesProvider).when(
+        data: (baxHistories) {
+          return ListView.builder(
+            itemCount: baxHistories.length,
+            itemBuilder: (context, index) {
+              final history = baxHistories[index];
+              ListTile(
+                title: Text('付与ポイント: '),
+              );
+            },
+          );
+        },
+        error: (error, stackTrace) {
+          return Container(
+            width: double.infinity,
+            height: 80,
+            color: Colors.white,
+            child: const Padding(
+              padding: EdgeInsets.all(12),
+              child: Text('エラーが発生しました。\n時間を置いて再度お試しください。'),
+            ),
+          );
+        },
+        loading: () {
+          return const Padding(
+            padding: EdgeInsets.all(12),
+            child: BaxIndicator(),
+          );
+        },
+      ),
     );
   }
 }
