ae6bfae4a1ee15fb025394e6788f36c74a2ef055
Kei Fujikawa <fujikei22funk@gmail.com>
2023-04-10 07:30:04 +0900

    BAX付与理由:  [object Object] となってしまうのを修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
-import * as firebaseAdmin from "firebase-admin";
-import * as functions from "firebase-functions";
+import axios from 'axios';
+import * as firebaseAdmin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 /**
  * Firestore Admin SDK の初期化
@@ -10,22 +10,24 @@ firebaseAdmin.initializeApp();
 const SLACK_WEBHOOK_URL = functions.config().slack.webhook_url;
 
 // Firestoreのリクエストコレクションに新規データが追加された時のトリガー
-export const sendNotificationToSlack = functions.firestore
-    .document("bax/{baxId}")
-    .onCreate(async (snapshot, _) => {
+export const sendNotificationToSlack = functions.firestore.document('bax/{baxId}').onCreate(async (snapshot, _) => {
     // 新規データを取得
-      const baxData = snapshot.data();
+    const data = snapshot.data();
+    const baxReasons = data.baxReasons;
+    const firstReason = baxReasons[0];
+    const text = firstReason.text;
+    const point = firstReason.point;
 
-      // Slackに送信するメッセージを作成
-      const message = `BAX付与理由:\n ${baxData.baxReasons}\n`;
+    // Slackに送信するメッセージを作成
+    const message = `BAX付与:\n ${text}\n${point}BAX`;
 
-      try {
-      // Slackに通知を送信
+    try {
+        // Slackに通知を送信
         await axios.post(SLACK_WEBHOOK_URL, {
-          text: message,
+            text: message,
         });
-        console.log("Slack通知が送信されました");
-      } catch (error) {
-        console.error("Slack通知の送信に失敗しました:", error);
-      }
-    });
+        console.log('Slack通知が送信されました');
+    } catch (error) {
+        console.error('Slack通知の送信に失敗しました:', error);
+    }
+});
