a3e02914c8241f413bac686e503fa79e21fc01b0
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-04-11 12:41:28 +0900

    フォーマット修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,54 +12,56 @@ firebaseAdmin.initializeApp();
 const SLACK_WEBHOOK_URL = functions.config().slack.webhook_url;
 
 /// BAXのcollectionのdoc作成でトリガーする
-export const sendNotificationToSlack = functions.firestore.document("bax/{baxId}").onCreate(async (snapshot, _) => {
+export const sendNotificationToSlack = functions.firestore
+  .document("bax/{baxId}")
+  .onCreate(async (snapshot, _) => {
     const bax = snapshot.data() as Bax;
     const baxReasons = bax.baxReasons;
     const firstReason = baxReasons[0];
     const text = firstReason.text;
-    const point: number = firstReason.point;
+    const point = firstReason.point;
 
     // baxを使った場合のみ通知したいので、プラスのpointの時はreturnする
     if (point > 0) {
-        return;
+      return;
     }
 
     // Slackに送信するメッセージを作成
     const message = `💰 BAX取引:\n ${text}\n${point}BAX`;
 
     try {
-        // Slackに通知を送信
-        await axios.post(SLACK_WEBHOOK_URL, {
-            text: message,
-        });
-        console.log("bax-slack通知が送信されました");
+      // Slackに通知を送信
+      await axios.post(SLACK_WEBHOOK_URL, {
+        text: message,
+      });
+      console.log("bax-slack通知が送信されました");
     } catch (error) {
-        console.error("bax-slack通知の送信に失敗しました:", error);
+      console.error("bax-slack通知の送信に失敗しました:", error);
     }
-});
+  });
 
 /// wifiが計測されたらトリガーする
 export const sendMeasureWiFiNotificationToSlack = functions.firestore
-    .document("wifiMeasurementResult/{resultId}")
-    .onCreate(async (snapshot, _) => {
-        const result = snapshot.data() as WifiMeasurementResult;
-        const placeId = result.placeId;
-        const ssid = result.ssid;
-        const downloadSpeedMbps = result.downloadSpeedMbps;
-        const uploadSpeedMbps = result.uploadSpeedMbps;
+  .document("wifiMeasurementResult/{resultId}")
+  .onCreate(async (snapshot, _) => {
+    const result = snapshot.data() as WifiMeasurementResult;
+    const placeId = result.placeId;
+    const ssid = result.ssid;
+    const downloadSpeedMbps = result.downloadSpeedMbps;
+    const uploadSpeedMbps = result.uploadSpeedMbps;
 
-        const googleMapURL = `https://www.google.com/maps/search/?api=1&query=bax&query_place_id=${placeId}&hl=ja`;
+    const googleMapURL = `https://www.google.com/maps/search/?api=1&query=bax&query_place_id=${placeId}&hl=ja`;
 
-        // Slackに送信するメッセージを作成
-        const message = `📱 BAX計測\nssid: ${ssid}\nDownload: ${downloadSpeedMbps}\nUpload: ${uploadSpeedMbps}\n${googleMapURL}`;
+    // Slackに送信するメッセージを作成
+    const message = `📱 BAX計測\nssid: ${ssid}\nDownload: ${downloadSpeedMbps}\nUpload: ${uploadSpeedMbps}\n${googleMapURL}`;
 
-        try {
-            // Slackに通知を送信
-            await axios.post(SLACK_WEBHOOK_URL, {
-                text: message,
-            });
-            console.log("wifi-slack通知が送信されました");
-        } catch (error) {
-            console.error("wifi-slack通知の送信に失敗しました:", error);
-        }
-    });
+    try {
+      // Slackに通知を送信
+      await axios.post(SLACK_WEBHOOK_URL, {
+        text: message,
+      });
+      console.log("wifi-slack通知が送信されました");
+    } catch (error) {
+      console.error("wifi-slack通知の送信に失敗しました:", error);
+    }
+  });
